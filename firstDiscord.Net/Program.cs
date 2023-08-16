1c0436f0d9c9dfc2cb7dcf630d34b40f0985129c
Takuto <tiharu717@gmail.com>
2023-08-16 09:48:40 +0900

    非同期プログラムに変更した

---
+++ firstDiscord.Net/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static Task Main(string[] args) 
+        => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        
+    }
+}
