fc31aa2ed03c18b1d278f2a67a661b2ec5efba96
Takuto <tiharu717@gmail.com>
2023-08-16 14:42:18 +0900

    json取り出せれない

--- firstDiscord.Net/Program.cs
+++ firstDiscord.Net/Program.cs
@@ -3,6 +3,8 @@
 //Console.WriteLine("Hello, World!");
 
 using System.Net.Sockets;
+using System.Reflection;
+using System.Text.Json.Serialization;
 using Discord;
 using Discord.Net;
 using Discord.WebSocket;
@@ -13,17 +15,22 @@ public class Program
 {
     private DiscordSocketClient _client;
     private SocketGuild _guild;
+    private AppJson _appJson;
+    
+    [JsonPropertyName("token")] public string token { get; }
     public static Task Main(string[] args) 
         => new Program().MainAsync();
 
     public async Task MainAsync()
     {
+        _appJson = await AppJson.GetJson("app.json");
+        Console.WriteLine("ロード完了");
         _client = new DiscordSocketClient();
 
         _client.Log += Log;
         //  You can assign your bot token to a string, and pass that in to connect.
         //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        var token = "MTA4OTM1OTM3NTIxNjQxMDcxNA.Gcz6d5.f6jnl-vXK8oQ127Ti0Hk0Tj06x79JFJhIaC0Co";
+        var token = _appJson.Token;
 
         // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
         // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
@@ -61,4 +68,28 @@ public class Program
             Console.WriteLine(e);
         }
     }
+}
+
+class AppJson
+{
+    public string Token { get; set; }
+
+    public static async Task<AppJson> GetJson(string fileName)
+    {
+        string jsonString = "";
+        using (StreamReader sr = new StreamReader(new FileStream("app.json", FileMode.OpenOrCreate)))
+        {
+            // Read and display lines from the file until the end of
+            // the file is reached.
+            while (sr.Peek() > 0)
+            {
+                jsonString += sr.ReadLine();
+            }
+        }
+        //string jsonString = r.ReadToEnd();
+        Console.WriteLine("1");
+        AppJson m = JsonConvert.DeserializeObject<AppJson>(jsonString);
+        return m;
+    }
+
 }
