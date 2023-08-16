381e7edb4186b67923e64b7412f6d71e5c5b5dd2
Takuto <tiharu717@gmail.com>
2023-08-16 13:55:46 +0900

    guildを取得できた 起動したすぐに取得するんじゃなくて、何秒が待ってbotがオンラインになるまでまたなければいけない.

--- firstDiscord.Net/Program.cs
+++ firstDiscord.Net/Program.cs
@@ -2,8 +2,11 @@
 
 //Console.WriteLine("Hello, World!");
 
+using System.Net.Sockets;
 using Discord;
+using Discord.Net;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
 
 public class Program
@@ -17,19 +20,22 @@ public class Program
         _client = new DiscordSocketClient();
 
         _client.Log += Log;
-
         //  You can assign your bot token to a string, and pass that in to connect.
         //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        var token = "MTA4OTM1OTM3NTIxNjQxMDcxNA.GeO_FD.ezpyOxwU3BWhZMMl8XLYcMuh-Yw1zS_RmGx3lQ";
+        var token = "MTA4OTM1OTM3NTIxNjQxMDcxNA.Gcz6d5.f6jnl-vXK8oQ127Ti0Hk0Tj06x79JFJhIaC0Co";
 
         // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
         // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
         // var token = File.ReadAllText("token.txt");
         // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
+        //_client.Ready += firstSlashCommand;
+
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
-
+        await Task.Delay(50000);
+        SocketGuild guild = _client.GetGuild(1089360703120490618);
+        Console.WriteLine(_client.Guilds.Count);
         // Block this task until the program is closed.
         await Task.Delay(-1); 
     }
@@ -38,5 +44,23 @@ public class Program
     {
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
+    }
+
+    public async Task firstSlashCommand()
+    {
+
+        var guild = _client.GetGuild(10893607031204906180);
+        SlashCommandBuilder slashCommandBuilder = new SlashCommandBuilder();
+        slashCommandBuilder.WithDescription("これは初めて作ったslashcommand。\nこれを実行すると、botが挨拶してくれます。");
+        slashCommandBuilder.WithName("sayhello");
+        Console.WriteLine(guild.Name);
+        try
+        {
+            await guild.CreateApplicationCommandAsync(slashCommandBuilder.Build());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
     }
 }
