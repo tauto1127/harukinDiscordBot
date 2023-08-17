e8f7b22d1793641e7a0c8d429b8e1ec2fec5efcb
Takuto <tiharu717@gmail.com>
2023-08-17 17:01:03 +0900

    テレポートパイプテーブルにタイプ追加

---
+++ HarukinDiscordBot/Migrations/20230817080015_add TeleportPipeType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace firstDiscord.Net.Migrations
+{
+    public partial class addTeleportPipeType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "TeleportChannels",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "TeleportChannels");
+        }
+    }
+}
--- HarukinDiscordBot/Model/TeleportChannel.cs
+++ HarukinDiscordBot/Model/TeleportChannel.cs
@@ -6,6 +6,7 @@ public class TeleportChannel
     public string Name { get; set; }
     public string Description { get; set; }
     public int Channel { get; set; }
+    public string Type { get; set; }
     public DateTime Created { get; set; }
     public DateTime Modified { get; set; }
 }
